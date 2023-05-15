d159b0f3b73a92b746425ac3e53a55aa10912798
valentina-sierrat <123768493+valentina-sierrat@users.noreply.github.com>
2023-05-15 12:43:45 -0500

    avance 2

---
+++ app.js
@@ -0,0 +1,24 @@
+const cuadrado=document.querySelector(".cuadrado");
+const tiempofaltante=document.querySelector("#tiempo");
+let puntaje=document.getElementById("puntaje");
+
+let resultado=0;
+let tiempoactual=tiempofaltante.textContent;
+
+function cuadroazar {
+  cuadrado.forEach(nombredeclase=>){
+    nombredeclase.classList.remove("topo");
+  })
+  let posicionalazar=cuadrado[Math.floor(Math.random()* 9)]
+  posicionalazar.classList.add("topo");
+  posiciontopo.posicionalazar.Id
+}
+cuadrado.forEach(identificador=>){
+  identificador.addEvenListener("click"()=>){
+    if(identificador.Id===posiciontopo){
+      resultado=resultado+1;
+      puntaje.textContent=resultado;
+      posiciontopo=null;
+    }
+  })
+})
